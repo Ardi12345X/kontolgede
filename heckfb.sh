655814e30b5eae98ef848e524e28e08375bdd5dc
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-04-07 20:01:46 +0700

    Add files via upload

---
+++ heckfb.sh
@@ -0,0 +1,19 @@
+sleep 1
+echo -e "\e[92msc heck akun fb target !\e[0m";
+sleep 1
+clear
+sleep 1
+echo -e "\e[93mNCI=NEW CYBER INDONESIA\e[0m";
+sleep 1
+clear
+sleep 2
+echo -e "\e[91m-----------------------------------------------------------------------------------\e[0m";
+sleep 1
+echo -e "\e[94mscript ini sudah tidak aktif !\e[0m";
+sleep 1
+echo -e "\e[93m-----------------------------------------------------------------------------------\e[0m";
+sleep 1
+echo -e "\e[96mketik exit untuk keluar !\e[0m";
+read -p '$>' userinput1;
+#
+sleep 4
